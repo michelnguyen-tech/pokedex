9888a0032eb311ff3098ea1864ded9d866d8e49b
Michel Nguyen <michelnguyenm9@gmail.com>
2021-04-01 11:29:12 -0400

    fourth commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 29
+var pokemon_count = 8;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -20,9 +20,37 @@ const colors = {
 const main_types = Object.keys(colors);
 
 const fetchPokemons = async ()  => {
-    for(let i = 1; i <= pokemon_count; i++ ) {
+    for(let i = pokemon_count-7; i <= pokemon_count; i++ ) {
         await getPokemon(i)
     }
+}
+
+const range = document.getElementById('range');
+
+range.addEventListener('input', (e) => {
+    const value = +e.target.value;
+    const label = e.target.previousElementSibling ;
+    pokemon_count = value;
+
+    const range_width = getComputedStyle(e.target).getPropertyValue('width');
+
+    const label_width = getComputedStyle(label).getPropertyValue('width');
+
+    const num_width = +range_width.substring(0, range_width.length - 2);
+    const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+    const max = +e.target.max;
+    const min = +e.target.min;
+
+    const left = ((value+48) * (num_width / max ) - num_label_width / 2 + scale((value+48), min, max, 10, -10));
+
+    label.style.left = `${left}px`;
+
+    label.innerHTML = value;
+})
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 const getPokemon = async (id) => {
@@ -72,5 +100,13 @@ const createPokemonCard = (pokemon) => {
 
     poke_container.appendChild(pokemonEl)
 }
+
+function removeCardsElt() {
+
+    var elements = document.getElementsByClassName("pokemon-card");
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+  }
 
 fetchPokemons()
